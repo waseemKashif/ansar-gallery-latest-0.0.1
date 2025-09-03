addc333455a377ee36148aa8865e81b1377b84aa
waseemkashif <waseemkashif7@gmail.com>
2025-09-03 14:17:28 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["https://ansargallery.com"], // Replace with your actual domain
+    formats: ["image/webp", "image/avif"],
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
 };
 
 export default nextConfig;
